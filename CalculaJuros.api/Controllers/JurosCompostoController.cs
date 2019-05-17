78987ffc9ecd8256657df795fe62d0107d96af46
Gustavo Lopes Azevedo <gustavo.1895@hotmail.com>
2019-05-16 22:55:13 -0300

    Ajuste nomeclatura solution e projects

---
+++ CalculaJuros.api/Controllers/JurosCompostoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculaJuros.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JurosCompostoController : ControllerBase
+    {
+        [HttpGet()]
+        public ActionResult<string> CalculaJuro()
+        {
+            return "teste 3";
+        }
+    }
+}
